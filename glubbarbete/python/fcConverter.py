9560fee92d11759051ff3c45801f4eddf202b075
Jonathan Hollstein <Cathronau@gmail.com>
2024-12-05 15:26:14 +0100

    initial commit

---
+++ glubbarbete/python/fcConverter.py
@@ -0,0 +1,17 @@
+def FToC():
+    fahrenheit = int(input("Input fahrenheit and ill convert it to celsius: "))
+    celsius = (fahrenheit - 32) / 1.8
+    print(f"Celsius: {celsius:.2f} ")
+
+def CToF():
+    celsius = int(input("Input celsius and ill convert it to fahrenheit: "))
+    fahrenheit = (celsius * 1.8) + 32
+    print(f"Fahrenheit: {fahrenheit:.2f}")
+
+opt = input("Fahrenheit to Celsius or Celsius to Fahrenheit: ")
+
+while(opt != "Fahrenheit" and opt != "Celsius"): 
+    opt = int(input("Invalid input, please try again: "))
+
+if(opt == "Fahrenheit"): FToC()
+else: CToF()
