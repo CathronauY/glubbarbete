6a1cb35b74bb741e28282b02ec51c367ad357bb3
Lispilo <96479509+Lispilo@users.noreply.github.com>
2025-04-29 10:50:21 +0200

    Create calculator_comandline.py

---
+++ python/calculator_comandline.py
@@ -0,0 +1,17 @@
+import math
+
+def calc_add(num1: int, num2: int) -> int:
+  answer = num1 + num2
+  return answer
+
+def calc_subtract(num1: int, num2: int) -> int:
+  answer = num1 - num2
+  return answer
+
+def calc_times(num1: int, num2: int) -> int:
+  answer = num1 * num2
+  return answer
+
+def calc_devide(num1: int, num2: int) -> int:
+  answer = num1 / num2
+  return answer
